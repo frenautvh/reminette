b07cd533a988be45f5c5884ab17fa39c2d4d7968
Florian Renaut <florian.renaut@corp.ovh.com>
2019-03-07 14:29:38 +0100

    add remcalc command

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const glob = require('glob');
 const path = require("path");
 const program = require('commander');
 
+const multiplyRems = function (files, regex, coeff) {
+  files.forEach(function (file) {
+    const src = fs.readFileSync(file).toString("utf-8");
+    const result = src.replace(regex, function (match, contents) {
+      const rem = parseFloat(contents, 10);
+      const result = `${rem * coeff}rem`;
+      return `${rem * coeff}rem`;
+    });
+    fs.writeFileSync(file, result, "utf-8");
+  });
+};
+
 program
   .command("multiply <sourcedir>")
   .alias("m")
@@ -16,18 +28,24 @@ program
       process.exit(1);
     }
     glob(path.join(sourcedir, "**/*.less"), function (err, files) {
-      files.forEach(function (file) {
-        const src = fs.readFileSync(file).toString("utf-8");
-        var count = 0;
-        const result = src.replace(/([+-]?[0-9]+\.?[0-9]*)rem/g, function (match, contents) {
-          const rem = parseFloat(contents, 10);
-          const result = `${rem * coefficient}rem`;
-          count++;
-          return `${rem * coefficient}rem`;
-        });
-        console.log(` > ${path.relative(sourcedir, file)}, replaced ${count} rem units`);
-        fs.writeFileSync(file, result, "utf-8");
-      });
+      multiplyRems(files, /([+-]?[0-9]+\.?[0-9]*)rem/g, coefficient);
+      console.log("done");
+    });
+  });
+
+program
+  .command("remcalc <sourcedir>")
+  .alias("r")
+  .option("-c, --coefficient <m>", "Coefficient applyed when replacing rem-calc", parseFloat)
+  .action(function (sourcedir, options) {
+    const coefficient = parseFloat(options.coefficient);
+    if (isNaN(options.coefficient)) {
+      console.error('Coefficient should be a valid float number');
+      process.exit(1);
+    }
+    glob(path.join(sourcedir, "**/*.less"), function (err, files) {
+      multiplyRems(files, /rem-calc\(([+-]?[0-9]+\.?[0-9]*)\)/g, coefficient);
+      console.log("done");
     });
   });
 
--- readme.md
+++ readme.md
@@ -5,5 +5,16 @@
 Multiply each rem units in less files contained in *sourcedir* by 0.625 coefficient.
 
 ```bash
+# 1rem -> 0.625rem
 reminette multiply -c 0.625 <sourcedir>
 ```
+
+Replace rem-calc in less files contained in *sourcedir* by 0.625 coefficient.
+
+```bash
+# rem-calc(1) -> 0.625rem
+reminette remcalc -c 0.625 <sourcedir>
+```
+
+### Caution
+If you fix remcalc first, don't forget that calling the multiply command later will apply the coefficient on each rem units. So you might apply coefficient twice.
